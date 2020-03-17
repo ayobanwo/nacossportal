3659f771152c427ad7f633df0fa311fd9fa8368c
Lightsource-Pris <59650945+Lightsource-Pris@users.noreply.github.com>
2020-03-18 06:14:20 +1030

    Favicon Update

--- contact.php
+++ contact.php
@@ -13,6 +13,7 @@
 	<link rel="stylesheet" type="text/css" href="css/navfoot.css">
 	<link rel="stylesheet" type="text/css" href="css/contact.css">
 	<link rel="stylesheet" type="text/css" href="css/floating-labels.css">
+	<link rel="icon" href="images/favicon.jpg">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
